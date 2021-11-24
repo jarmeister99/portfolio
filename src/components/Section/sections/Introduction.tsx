e8669a8d4daaed23b90f98aaf54fbe135009b9d2
jarmeister <jarmeister99@gmail.com>
2021-11-23 19:06:11 -0800

    bugfix on closeparagraph component and added introduction

--- src/components/Section/sections/Introduction.tsx
+++ src/components/Section/sections/Introduction.tsx
@@ -1,8 +1,26 @@
+import { CloseParagraph } from "../../generic/typography";
 
 const IntroductionSection: React.FC = () => {
     return (
         <div>
-            <p>A basic intro about me</p>
+            <CloseParagraph fontSize={0} spacing={3}>
+                My name is Jared Foster, and I currently work as an Embedded Software Engineer in Morro Bay, California. I graduated California Polytechnic State
+                University, San Luis Obispo in June of 2017.
+            </CloseParagraph>
+            <CloseParagraph fontSize={0} spacing={3}>
+                I greatly enjoy learning new languages. Professionally, I use C, Python, and AVR Assembly. Outside of work, I build projects with Python and JavaScript (and recently TypeScript).
+            </CloseParagraph>
+            <CloseParagraph fontSize={0} spacing={3}>
+                I have experience in low-level firmware development, test/task automation, and full-stack web development.
+            </CloseParagraph>
+            <CloseParagraph fontSize={0} spacing={3}>
+                I have built several web applications using the MERN stack. All of my projects are hosted using a VPS (either AWS LightSail or DigitalOcean). I have also built a couple
+                web applications using Flask.
+            </CloseParagraph>
+            <CloseParagraph fontSize={0} spacing={3}>
+                I have built several web applications using the MERN stack. All of my projects are hosted using a VPS (either AWS LightSail or DigitalOcean). I have also built a couple
+                web applications using Flask.
+            </CloseParagraph>
         </div>
     )
 }
--- src/components/generic/typography.tsx
+++ src/components/generic/typography.tsx
@@ -2,17 +2,28 @@ import styled from "styled-components";
 
 interface ICloseParagraph {
     fontSize?: number;
+    spacing?: number;
 }
 const CloseParagraph = styled.p<ICloseParagraph>`
-    margin: 0.15em 0 0.15em 0;
-    font-size: ${props => {
-        const base = 14;
-        if (props.fontSize){
-            const calculatedFontSize = base + (props.fontSize * 2)
-            return `${calculatedFontSize}px`;
+    margin: ${props => {
+        const base = 0.15;
+        const increase = 0.30;
+        if (props.spacing){
+            const calculatedSpacing = base + (props.spacing * increase)
+            return `${calculatedSpacing}em 0 ${calculatedSpacing}em 0;`;
         }
         else{
-            return `${base}px`
+            return `${base}em 0 ${base}em 0;`
+        }
+    }}
+    font-size: ${props => {
+        const base = 16;
+        if (props.fontSize){
+            const calculatedFontSize = base + (props.fontSize * 2)
+            return `${calculatedFontSize}px;`;
+        }
+        else{
+            return `${base}px;`
         }
     }}
 `;
