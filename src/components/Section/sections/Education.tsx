f1ac7b941b1089b6609473cf700dfd5e5984008a
Jared Foster <jfoster@rantec.com>
2021-11-23 16:43:31 -0800

    changed education style a bit

--- src/components/Section/sections/Education.tsx
+++ src/components/Section/sections/Education.tsx
@@ -4,7 +4,7 @@ const EducationSection: React.FC = () => {
     return (
         <div>
             <CloseParagraph fontSize={1}>California Polytechnic State University, San Luis Obispo</CloseParagraph>
-            <CloseParagraph>Computer Engineering - 3.4 GPA</CloseParagraph>
+            <CloseParagraph>B.S. Computer Engineering - <b>3.4 GPA</b></CloseParagraph>
             <CloseParagraph><i>September 2017 - June 2021</i></CloseParagraph>
             <CloseParagraph fontSize={-1}>2x President's List, 5x Dean's List</CloseParagraph>
         </div>
